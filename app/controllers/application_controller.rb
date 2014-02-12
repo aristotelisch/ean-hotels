595a057cc0e604d50b910482a9503a7743979050
Aristotelis Christou <telischristou@gmail.com>
2014-02-12 14:23:23 +0200

    Fix2 use one memcached server

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   # use the api with a default parameter if empty
   def get_hotels_list_response(search_parameters = { :destinationString => 'new york' })
       Rails.cache.fetch search_parameters, :expires_in => 5.minutes do
-        sleep 2.seconds
+        sleep 3.seconds
         api = Expedia::Api.new
       # Method to search for a hotel. see http://developer.ean.com/docs/read/hotels/version_4/request_hotel_list
       # {:propertyName => 'Hotel Moa Berlin', :destinationString => 'berlin'}
